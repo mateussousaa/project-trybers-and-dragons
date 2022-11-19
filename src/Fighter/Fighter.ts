38010eac72bff6b1b1408f40ff07d6b8401883d7
mateussousaa <mateussousa20@outlook.com>
2022-11-19 11:45:14 -0300

    feat: creating the interface SimpleFighter - requirement 08

---
+++ src/Fighter/Fighter.ts
@@ -0,0 +1,10 @@
+import Energy from '../Energy';
+import SimpleFighter from './SimpleFighter';
+
+export default interface Fighter extends SimpleFighter {
+  defense: number,
+  energy?: Energy,
+
+  special?(enemy: Fighter): void
+  levelUp(): void
+}
